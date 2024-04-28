62c9a2c0eb54524703ad09535e25fe4fcbdccfc4
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-04-28 17:23:31 +0600

    deploy complete

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const spotsCollection = client.db('TouristSpot').collection('spots')
     
@@ -89,7 +89,7 @@ app.delete('/mylist/:id',async(req,res)=>{
   res.send(result);
 })
 
-    await client.db("admin").command({ ping: 1 });
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
