012910905ba05fcffb29a63f53c47669a6da30ae
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-04-28 12:00:17 +0600

    delete added

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ app.put('/mylist/:id',async(req,res)=>{
   res.send(result)
 })
 
+app.delete('/mylist/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await spotsCollection.deleteOne(query);
+  res.send(result);
+})
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
