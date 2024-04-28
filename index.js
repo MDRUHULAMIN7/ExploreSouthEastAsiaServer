18ab6770ff247bb44174d152b9ea27c67fea2eed
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-04-28 22:39:32 +0600

    country posted

--- index.js
+++ index.js
@@ -32,6 +32,44 @@ async function run() {
     // await client.connect();
 
     const spotsCollection = client.db('TouristSpot').collection('spots')
+
+    const countryCollection =client.db('Country').collection('countries')
+    
+    const fakeData = [
+      {
+        image: "https://via.placeholder.com/300",
+        title: "Lorem Ipsum",
+        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
+      },
+      {
+        image: "https://via.placeholder.com/300",
+        title: "Nulla facilisi",
+        details: "Nulla facilisi. Nullam in magna. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam."
+      },
+      {
+        image: "https://via.placeholder.com/300",
+        title: "Vestibulum ante ipsum",
+        details: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sollicitudin velit sed leo. Ut pharetra augue nec augue."
+      },
+      {
+        image: "https://via.placeholder.com/300",
+        title: "Mauris lacinia lectus",
+        details: "Mauris lacinia lectus. Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus. Donec sit amet nisl."
+      },
+      {
+        image: "https://via.placeholder.com/300",
+        title: "Phasellus faucibus molestie",
+        details: "Phasellus faucibus molestie nisl. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."
+      }
+    ];
+    
+   
+    app.post('/countries',async(req,res)=>{
+      const fakeData = req.body;
+      console.log(fakeData);
+      const  result = await countryCollection.insertMany(fakeData);
+      res.send(result)
+    })
     
 
     app.get('/spots',async(req,res)=>{
