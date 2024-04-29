77ee8c9df9c88dc7688d6638e5ad390a6fdc9cf7
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-04-29 18:02:07 +0600

    another collection

--- index.js
+++ index.js
@@ -32,42 +32,24 @@ async function run() {
     // await client.connect();
 
     const spotsCollection = client.db('TouristSpot').collection('spots')
+    const countryDataCollection = client.db('CountryData').collection('countrydata')
 
-    const countryCollection =client.db('Country').collection('countries')
-    
-    const fakeData = [
-      {
-        image: "https://via.placeholder.com/300",
-        title: "Lorem Ipsum",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
-      },
-      {
-        image: "https://via.placeholder.com/300",
-        title: "Nulla facilisi",
-        details: "Nulla facilisi. Nullam in magna. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam."
-      },
-      {
-        image: "https://via.placeholder.com/300",
-        title: "Vestibulum ante ipsum",
-        details: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sollicitudin velit sed leo. Ut pharetra augue nec augue."
-      },
-      {
-        image: "https://via.placeholder.com/300",
-        title: "Mauris lacinia lectus",
-        details: "Mauris lacinia lectus. Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus. Donec sit amet nisl."
-      },
-      {
-        image: "https://via.placeholder.com/300",
-        title: "Phasellus faucibus molestie",
-        details: "Phasellus faucibus molestie nisl. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."
-      }
-    ];
+    const countryCollection =client.db('TouristSpot').collection('countries')
     
    
-    app.post('/countries',async(req,res)=>{
-      const fakeData = req.body;
-      console.log(fakeData);
-      const  result = await countryCollection.insertMany(fakeData);
+    
+   
+ 
+
+    app.get('/countrydata',async(req,res)=>{
+      const cursor = countryDataCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    
+    app.get('/countries',async(req,res)=>{
+      const cursor = countryCollection.find();
+      const result = await cursor.toArray();
       res.send(result)
     })
     
